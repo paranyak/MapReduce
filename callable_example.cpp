3bcfb2a17b2566158dde0651bf3bf2ab926110d5
natasha <paranyak@ucu.edu.ua>
2017-05-04 15:56:00 +0300

    changed func_tmpl

--- callable_example.cpp
+++ callable_example.cpp
@@ -8,6 +8,19 @@ auto func_tmpl(const X& m, Y fn1, Z fn2) -> decltype(fn2(fn1(m))) {
     // x - vector
     // y - map
     // z - map
+    //кількість потоків
+    int n;
+    bool cond;
+    do {
+        cout<< "How many threads do you want to run? ";
+        cin >> n;
+        cond = cin.fail();
+        cin.clear();
+        cin.ignore(INT_MAX, '\n');
+    }while(cond);
+    cout<< n;
+    //
+    //cout<< m.size() << "SSSSSSS" ;
     auto result = fn1(m); //приймає vector<string>words
     auto result2 = fn2(result);
     return result2; // fn2(result)
@@ -17,7 +30,7 @@ auto func_tmpl(const X& m, Y fn1, Z fn2) -> decltype(fn2(fn1(m))) {
 void *callE(const vector<string>& words){
     std::map<string, int> a;
     //a.insert(pair<string, int>("a", 2));
-    a = func_tmpl(words, counting_words_worker, result);
+    func_tmpl(words, counting_words_worker, result);
 }
 
 
--- counting.cpp
+++ counting.cpp
@@ -9,15 +9,15 @@ map<string, int> localm;
 
 
 map<string, int> counting_words_worker(vector<string> words){
-    map<string, int> localm;
+
     for (int i = 0; i != words.size(); ++i) {
-        if(localm.find(words[i]) == localm.end()){
-            localm[words[i]] = 1;
-        }else{
-            localm[words[i]] +=1;
+            if(localm.find(words[i]) == localm.end()){
+                localm[words[i]] = 1;
+            }else{
+                localm[words[i]] +=1;
+            }
         }
-    }
-    return localm;
+        return localm;
 }
 
 map<string, int> result ( map<string, int> localm) {
