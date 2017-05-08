3bd47f63a411301bacca597a689fbb05ba9509ef
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-05-08 16:08:30 +0300

    Update callable_example.cpp

--- callable_example.cpp
+++ callable_example.cpp
@@ -1,37 +1,56 @@
 #include <iostream>
+#include <map>
+#include <vector>
+
+//#include <mutex>
+
 
 using namespace std;
 
-
-template <typename X, typename Y, typename Z>
-auto func_tmpl(const X& m, Y fn1, Z fn2) -> decltype(fn2(fn1(m))) {
-    // x - vector
-    // y - map
-    // z - map
-    //кількість потоків
-    int n;
-    bool cond;
-    do {
-        cout<< "How many threads do you want to run? ";
-        cin >> n;
-        cond = cin.fail();
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-    }while(cond);
-    cout<< n;
-    //
-    //cout<< m.size() << "SSSSSSS" ;
-    auto result = fn1(m); //приймає vector<string>words
-    auto result2 = fn2(result);
-    return result2; // fn2(result)
+map<string, int> counting_words_worker(const vector<string>& words){
+    map<string, int> localm;
+    for (size_t  i = 0; i != words.size(); ++i) {
+            ++localm[words[i]];
+    }
+    return localm;
 }
 
+map<string, int> reduce_f(map<string, int> m, const map<string, int>& localm) {
+    for (auto it = localm.begin(); it != localm.end(); ++it)
+        m[it->first] += it->second;
 
-void *callE(const vector<string>& words){
-    std::map<string, int> a;
-    //a.insert(pair<string, int>("a", 2));
-    func_tmpl(words, counting_words_worker, result);
+    return m;
 }
 
+template <class V, class MF, class RF>
+auto func_tmpl(const V& v, MF fn1, RF fn2) ->
+            decltype( fn2(fn1(v[0]), fn1(v[0])) ) {
+    // V - масив блоків (векторів)
+    decltype(func_tmpl(v, fn1, fn2)) res;        // big map<string, int>
+    for (size_t i = 0; i < v.size(); ++i) {
+        auto x = fn1(v[i]);        //return small   map<string, int>
+        res = fn2(move(res), x);
+    }
+    for (auto i : res){ 
+        cout << i.first << " - " << i.second << endl;
+    }
+    return res;
+}
 
+void callE(const vector<vector<string>> &words){
+    func_tmpl(words, counting_words_worker, reduce_f);
+}
 
+int main()
+{
+    vector<string> v1 = {"asdf", "wert", "aaa", "qwerty"};
+    vector<string> v2 = {"asdf", "poiuy", "qwewrtdxfb", "qwerty"};
+    vector<vector<string>> v3;
+    v3.push_back(v1);
+    v3.push_back(v2);
+    callE(v3);
+
+}
+
+// template <class T>  T reduce_f(T m, const T& localm)
+// func_tmpl(words, counting_words_worker, reduce_f<map<string, int>>);
