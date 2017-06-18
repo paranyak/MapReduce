6d7e98d3bd886c8c0c8d631b5cb35d46856834b0
natasha <paranyak@ucu.edu.ua>
2017-06-18 10:55:24 +0300

    fixed problems with integral

--- map_reduce.cpp
+++ map_reduce.cpp
@@ -5,47 +5,18 @@
 #include <assert.h>
 #include <thread>
 #include <mutex>
-#include <condition_variable>
 #include <deque>
+#include <condition_variable>
 
 
 using namespace std;
 mutex myMutex;
-mutex mx;
 condition_variable cv;
+
 int num = 0;
-int num_of_threads;
-deque <double> dm;
 
-double reduce_f(double & m, const double &localm ) {
-    m += localm;
-    return m;
-}
-
-void reducer(deque <double> &dm){
-    unique_lock<mutex> uniqueLock(myMutex);
-    while (dm.size() > 1) {
-        double map1 = dm.front();
-        dm.pop_front();
-        double map2 = dm.front();
-        dm.pop_front();
-        uniqueLock.unlock();
-        double map3 = reduce_f(map1, map2);
-        uniqueLock.lock();
-        dm.push_back(map3);
-        cout << "1A"  << " n " << num<<endl;
-    }if (dm.size() == 1 && num == num_of_threads) {
-       cout << dm.front() << endl;
-        cout << "2A" << endl;
-    } else {
-        cv.wait(uniqueLock);
-        cout << "3A" << endl;
-    }
-
-}
 
 double func_calculation(int m, double x1, double x2) {
-
     double sum1 = 0;
     double sum2 = 0;
     double g;
@@ -55,10 +26,43 @@ double func_calculation(int m, double x1, double x2) {
         sum2 += i * cos((i + 1) * x2 + 1);
     }
     g = - sum1 * sum2;
-    //cout << g  << " -g-- "<< endl;
-
     return g;
 }
+double thread_integration(double r, const double &sum) {
+    r += sum;
+    return r;
+}
+
+
+
+
+
+template <class Dd, class RF>
+Dd reducer_univ(int num_of_threads, deque <Dd> &dm, RF (*fn2)(Dd, const Dd &)){
+    unique_lock<mutex> uniqueLock(myMutex);
+    cout << dm.size() << " <- size of deque " << num << " <-- threads finished :) "<<endl;
+    while (dm.size() > 1) {
+        Dd map1 = dm.front();
+        dm.pop_front();
+        Dd map2 = dm.front();
+        dm.pop_front();
+        uniqueLock.unlock();
+        Dd map3 = (*fn2)(map1, map2);
+        uniqueLock.lock();
+        dm.push_back(map3);
+        cout << "1" << endl;
+    } cout << dm.size() << " <-size" << endl;
+    if (dm.size() == 1 && num == num_of_threads) {
+        cout << dm.front() << endl;
+        cout << "2" << endl;
+    } else {
+        cout << "3" << endl;
+        cv.wait(uniqueLock);
+    }
+
+}
+
+
 
 using point_t = pair<double, double>;
 
@@ -118,21 +122,18 @@ public:
 
     Iter2Ddouble& operator+=(size_t n)
     {
-        //! TODO: write without loop!
         for(size_t i = 0; i<n; ++i)
             ++(*this);
         return *this;
     }
     Iter2Ddouble& operator-=(size_t n)
     {
-        //! TODO: write without loop!
         for(size_t i = 0; i<n; ++i)
             --(*this);
         return *this;
     }
     ptrdiff_t operator-=(Iter2Ddouble itr)
     {
-        //! TODO: write without loop!
         ptrdiff_t diff = 0;
         if( *this < itr) {
             while (*this < itr) {
@@ -184,90 +185,88 @@ bool operator==(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2)
 struct func_wrapper
 {
     int m;
-    func_wrapper(int m_): m(m_){}
-
-    double operator()(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2)
+    deque<double> d;
+    func_wrapper(int m_, deque<double> d_): m(m_), d(d_){};
+//    double operator()(const point_t& p)
+//    {
+//        return func_calculation(m, p.first, p.second);
+//    }
+    double operator()(const Iter2Ddouble& itr1, const Iter2Ddouble& itr2,  deque<double>& d)
     {
+        cout <<"here wrapper" << endl;
         double r = 0;
-        for(auto i = itr1; i<itr2; ++i) {
+        for(auto i = itr1; i <itr2; ++i) {
             r += func_calculation(m, (*i).first, (*i).second) * itr1.get_pr() * itr1.get_pr();
-            {lock_guard<mutex> lg(myMutex);
-            dm.push_back(r); }
         }
-
-        cv.notify_one();
+        cout << "ended " << r << endl;
+        {lock_guard<mutex> lg(myMutex);
+            d.push_back(r);
+        }
         num += 1;
+        cout << "finished  " << num << endl;
+        std::thread::id this_id = std::this_thread::get_id();
+        std::cout << "thread " << this_id << "\n";
+        cv.notify_one();
         cv.notify_all();
-        reducer(dm);
-
+        reducer_univ(4, d, thread_integration);
         return r;
-
     }
 
 };
 
 
-double integration(const vector<double>& data ) {
-    double x0 = data[0];
-    double x = data[1];
-    double y0 = data[2];
-    double y = data[3];
-    double m = data[4];
-    double pr = data[5];
-
-    assert (m >= 5);
-    double sum = 0;
-    for (double i = x0; i <= x; i += pr) {
-        for (double j = y0; j <= y; j += pr) {
-            sum += func_calculation(m, i + pr / 2.0, j + pr / 2.0) * pr * pr;
-        }
-    }
-    //cout << sum << " " << endl;
-    return sum;
-}
-
-double thread_integration(double r, const double &sum) {
-    r += sum;
-    return r;
-}
 
 
 
-template <class MF, class RF, class I, class N, class D >
-auto func_tmpl(I beg, I fin, MF fn1, D d, RF fn2,  N num_of_threads) -> decltype( fn2(fn1(beg, fin), fn1(beg, fin)) ) {
 
-    decltype(func_tmpl(beg, fin, fn1, d, fn2, num_of_threads)) res;        // big map<string, int>
+
+
+
+
+
+
+template <class MF, class RF, class I, class N, class Dd>
+Dd func_tmpl(I beg, I fin, MF fn1, deque<Dd> &d, RF fn2, N num_of_threads) {
+
+    Dd res;
     size_t delta = (fin - beg) / num_of_threads;
     vector<I> segments;
     for (auto i = beg; i < fin - delta; i += delta) {
         segments.push_back(i);
     }
     segments.push_back(fin);
-//    for(auto x: segments)
-//    {
-//        cout << distance(beg, x) << "," ;
-//    }
 
-    vector<thread> threads; //потік
-
+    thread myThreads [num_of_threads];
     for (auto i = segments.begin(); i < segments.end()-1; ++i) {
-        threads.push_back(std::thread(fn1,*i, *(i+1)));
-        auto x = fn1(*i, *(i+1));
-        cout << x << endl;
+        myThreads[i - segments.begin()] = thread(fn1, *i, *(i+1), ref(d));
+        cout << "i: "<<i - segments.begin() << endl;
+        //reducer_univ(num_of_threads, d, fn2);
     }
 
-
-    for (auto& th : threads) th.join();
+    for (auto& th : myThreads){
+        if (th.joinable())
+        {th.join();
+        std::thread::id this_id = std::this_thread::get_id();
+        std::cout << "thread " << this_id << " ...\n";}
+    }
+    res = reducer_univ(num_of_threads, d, fn2);
     return res;
 }
 
-int main() {
-    vector<double> data = {0, 1, 0, 1, 5, 0.001};
+
+int main()
+{
+//    deque <map<string, int>> dm;
+//    vector<string> v = {"aaaa", "bbbbb", "ccccc", "ddddd", "aaaa", "eeeee", "wwwwwwwww", "ccccc", "ccccc", "ccccc"};
+//    cout<< "Words counter" << endl;
+//    int num_of_threads = 3;
+//    map<string, int> m = func_tmpl(v.begin(), v.end(), counting_words_worker, dm, reduce_f, num_of_threads);
+//    printMap(m);
+
+    vector<double> data = {0, 2, 0, 4, 5, 0.001};
+    deque<double> d;
     Iter2Ddouble itr(data);
     cout << "Integral" << endl;
-    num_of_threads = 4;
-    deque <double> dm;
-    cout << func_tmpl(itr, itr.end(), func_wrapper(5), dm,  thread_integration, num_of_threads) << endl;
-
+    cout << func_tmpl(itr, itr.end(), func_wrapper(5, d), d, thread_integration, 4) << endl;
 
 }
